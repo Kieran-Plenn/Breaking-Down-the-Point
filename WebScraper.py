c2d960f87fd1771f1a2c497bd9b2c520b1b0fcb7
Kieran-Plenn <kieranplenn@gmail.com>
2025-04-08 19:31:23 -0400

    Set up requirements.txt, hid virutal environment, and used lxml instead.

--- WebScraper.py
+++ WebScraper.py
@@ -2,12 +2,12 @@
 # Author: Kieran Plenn
 
 import requests
-import html5lib
+import lxml
 from bs4 import BeautifulSoup
 import pandas as pd
 
 # First let's scrape the list of players
 player_list_url = "https://tennisabstract.com/reports/atpRankings.html"
 player_list_response = requests.get(player_list_url)
-player_list_soup = BeautifulSoup(player_list_response.content, 'html5lib')
+player_list_soup = BeautifulSoup(player_list_response.content, 'lxml')
 print(player_list_soup)
