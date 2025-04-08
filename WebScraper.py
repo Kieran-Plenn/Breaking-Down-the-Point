dbf2ef9c9705ad4dbe3af5eafcb6d3e31b1673ef
kieranplenn <tuk25850@temple.edu>
2025-04-08 12:29:48 -0400

    Web Scraper - successful html parsing for player list url

--- WebScraper.py
+++ WebScraper.py
@@ -2,12 +2,12 @@
 # Author: Kieran Plenn
 
 import requests
+import html5lib
 from bs4 import BeautifulSoup
 import pandas as pd
 
 # First let's scrape the list of players
 player_list_url = "https://tennisabstract.com/reports/atpRankings.html"
 player_list_response = requests.get(player_list_url)
-player_list_soup = BeautifulSoup(player_list_response, 'html.parse')
-print(player_list_response.content)
+player_list_soup = BeautifulSoup(player_list_response.content, 'html5lib')
 print(player_list_soup)
